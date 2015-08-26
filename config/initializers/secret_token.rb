5452da3907bb05f1ca4d3623bd0a9c4227470e3b
wala2.elsayed <walaa.elsayed@bibalex.org>
2015-08-26 09:55:53 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BhlRails4::Application.config.secret_key_base = '3177074c573f82963ab49dcc1d08aaea5e80b7813e3be8a73a9cb52762c4b126e205a32cc9e6c6d916a3bd29725de50d3e3779c171aa51201f5c2bb35ceb82c2'
