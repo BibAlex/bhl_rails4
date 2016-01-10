0d554f60c8f841c5a7a8cadc8a51a538d7b92731
wala2.elsayed <walaa.elsayed@bibalex.org>
2016-01-10 10:41:32 +0200

    update book_views table

--- app/models/book_view.rb
+++ app/models/book_view.rb
@@ -4,8 +4,7 @@ class BookView < ActiveRecord::Base
     if(session_book_id != nil && session_book_id != params[:id].to_i)
        if BookView.where(source_book_id: params[:id].to_i, dest_book_id: session_book_id).blank? && 
           BookView.where(source_book_id: session_book_id, dest_book_id: params[:id].to_i).blank?
-         source_book_title = BooksHelper.find_field_in_document(session_book_id, "title")[0]
-         BookView.create(source_book_id: session_book_id, dest_book_id: params[:id].to_i, source_book_title: source_book_title, dest_book_title: volume[:title][0])
+         BookView.create(source_book_id: session_book_id, dest_book_id: params[:id].to_i)
       end
     end    
   end
