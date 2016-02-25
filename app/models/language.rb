3ad1d401538244597e0f2a8c847e73ccc2abb17b
wala2.elsayed <walaa.elsayed@bibalex.org>
2016-02-25 10:25:04 +0200

    update language model: remove code length validation

--- app/models/language.rb
+++ app/models/language.rb
@@ -2,5 +2,5 @@ class Language < ActiveRecord::Base
   has_many :book_languages, dependent: :destroy
   has_many :books, through: :book_languages
   
-  validates :code, presence: true, length: { minimum: 3, maximum: 3 }
+  validates :code, presence: true
 end
