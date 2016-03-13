e57dac18894167f35b8f1b7dcdb58a3378eb98fd
wala2.elsayed <walaa.elsayed@bibalex.org>
2016-03-13 11:22:50 +0200

    accept books without languages

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -259,7 +259,7 @@ module BooksHelper
   def self.find_field_in_document(job_id, field)
     languages = { "English" => "en", "German" => "ge", "Arabic" => "ar", "French" => "fr", "Italian" => "it" }
     doc = SolrHelper.solr_find_document("job_id:#{job_id}")    
-    lang = (!doc["language_facet"].empty? && languages.has_key?(doc["language_facet"][0])) ? languages[doc["language_facet"][0]] : "ud"
+    lang = (!doc["language_facet"].blank? && languages.has_key?(doc["language_facet"][0])) ? languages[doc["language_facet"][0]] : "ud"
     doc["#{field}_#{lang}"]
   end
 
@@ -296,7 +296,7 @@ module BooksHelper
     unless solr_response["response"]["numFound"] == 0
       doc = solr_response["response"]["docs"][0]
       languages = { "English" => "en", "German" => "ge", "Arabic" => "ar", "French" => "fr", "Italian" => "it" }
-      lang = (!doc["language_facet"].empty? && languages.has_key?(doc["language_facet"][0])) ? languages[doc["language_facet"][0]] : "ud"
+      lang = (!doc["language_facet"].blank? && languages.has_key?(doc["language_facet"][0])) ? languages[doc["language_facet"][0]] : "ud"
       volume = { title: doc["title_#{lang}"], author: doc["author_#{lang}"], subject: doc["subject_#{lang}"],
                  rate: doc["rate"], views: doc["views"], job_id: doc["job_id"], date: doc["date"],
                  language: doc["language_facet"], location: doc["location_search"], publisher: doc["publisher_#{lang}"] }
@@ -319,7 +319,7 @@ module BooksHelper
         tmp = all_sci_names_with_facets[:sci_names][doc[:job_id]]
         sci_names = tmp.nil? ? [] : tmp
         languages = { "English" => "en", "German" => "ge", "Arabic" => "ar", "French" => "fr", "Italian" => "it" }
-        lang = (!doc["language_facet"].empty? && languages.has_key?(doc["language_facet"][0])) ? languages[doc["language_facet"][0]] : "ud"
+        lang = (!doc["language_facet"].blank? && languages.has_key?(doc["language_facet"][0])) ? languages[doc["language_facet"][0]] : "ud"
         options = { title: doc["title_#{lang}"], author: doc["author_#{lang}"], subject: doc["subject_#{lang}"],
                     rate: doc["rate"], views: doc["views"], job_id: doc["job_id"], date: doc["date"],
                     language: doc["language_facet"], location: doc["location_search"], publisher: doc["publisher_#{lang}"], sci_names: sci_names }
