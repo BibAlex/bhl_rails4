3bcf1cba90a7e0ff71de00a796b2164a76f39a05
Huda Soliman <huda.soliman@bibalex.org>
2016-03-30 11:09:22 +0200

    cleaning names#show method

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -1,22 +1,20 @@
 class NamesController < ApplicationController
-  
+
   include SolrHelper
-  
+
   def index
     @page_title = I18n.t('common.tree_of_life_title')
     @entries = Hierarchy.where(browsable: 1).order("label")
   end
-  
-  
+
+
   def show
-    parent_id = params[:id]
-    hierarchy_id = params[:h_id]
-    parent_id = 0 unless parent_id
-    hierarchy_id = DEFAULT_HIERARCHY_ID unless hierarchy_id
+    parent_id = params[:id] || 0
+    hierarchy_id = params[:h_id] || DEFAULT_HIERARCHY_ID
     @hierarchy_entries = HierarchyEntry.find_siblings(hierarchy_id, parent_id)
-    render :layout => 'main' # this is a blank layout as I don't need any layout in this action
+    render layout: 'main' # this is a blank layout as I don't need any layout in this action
   end
-  
+
   def get_content
     @id = params[:id]
     he = HierarchyEntry.find_taxon(@id)
@@ -28,22 +26,22 @@ class NamesController < ApplicationController
     if image_source.nil?
       @image = nil
     else
-      @image = EOL_CONTENT_PATH + image_source.to_s.from(0).to(3) + "/" +  
-                  image_source.to_s.from(4).to(1) + "/" + 
+      @image = EOL_CONTENT_PATH + image_source.to_s.from(0).to(3) + "/" +
+                  image_source.to_s.from(4).to(1) + "/" +
                   image_source.to_s.from(6).to(1) + "/" +
                   image_source.to_s.from(8).to(1) + "/" +
                   image_source.to_s.from(10) + "_260_190.jpg"
     end
-    
+
     unless @name.blank?
       @name.gsub('"', '\"')
       @books_count = get_volumes_contain_sci_name([@name], " OR ").count
     else
       @books_count = 0
     end
-    
+
     render :layout => 'main' # this is a blank layout as I don't need any layout in this action
   end
 
-  
+
 end
