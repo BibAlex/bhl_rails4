a7048221139f8aa081fcc97bc51bf8eabfee9634
wala2.elsayed <walaa.elsayed@bibalex.org>
2016-05-08 10:43:46 +0200

    Updating Assets configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ BhlRails4::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_files = false
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.compress = true
@@ -31,7 +31,10 @@ BhlRails4::Application.configure do
   config.assets.compile = true
 
   # Generate digests for assets URLs.
-  config.assets.digest = false
+  config.assets.digest = true    
+  config.assets.handle_expiration = true
+  config.assets.expire_after 2.months
+
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.1'
