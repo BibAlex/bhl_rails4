5ba3527cb0e665fe469a9223ec444edfe6131522
wala2.elsayed <walaa.elsayed@bibalex.org>
2016-12-08 08:56:05 +0200

    change password: update angular controller

--- app/assets/javascripts/users/change_password.js
+++ app/assets/javascripts/users/change_password.js
@@ -1,4 +1,4 @@
-bhlApp.controller('userValidate', function($scope, $window) {
+bhlApp.controller('userValidateChangePaaword', function($scope, $window) {
     $scope.showErrors = false;
     $scope.validateForm = function(event, userForm) {
         if (userForm.$invalid) {
