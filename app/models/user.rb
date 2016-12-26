dc86a29ab8b741b00d624fc75528140b77203bde
wala2.elsayed <walaa.elsayed@bibalex.org>
2016-12-26 11:30:40 +0200

    Fixing Failed Login Flash Message

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,7 +65,7 @@ class User < ActiveRecord::Base
   def file_size
     unless photo_name.blank?
       if photo_name.file.size.to_f/(1000*1000) > UPLOADED_PHOTO_MAX_SIZE
-        errors.add(:file, "#{I18n.t('warn.upload_photo_max_size_error')} #{UPLOADED_PHOTO_MAX_SIZE} #{I18n.t('common.mb')}")
+        errors.add(:file, "#{I18n.t('msgs.upload_photo_max_size_error')} #{UPLOADED_PHOTO_MAX_SIZE} #{I18n.t('common.mb')}")
       end
     end
   end
